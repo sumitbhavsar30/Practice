b41eff7e575b297f79f83fad3e758f295f802e72
sumitbhavsar730 <sbhavsar730@gmail.com>
2021-03-29 23:42:59 +0530

    1 java file added

---
+++ src/main/java/myproject1/StringInput.java
@@ -0,0 +1,18 @@
+package myproject1;
+
+import java.util.*;
+
+public class StringInput {
+
+	
+	
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+	
+	String Str = sc.nextLine();
+	
+	System.out.println(Str);
+	
+	}
+}
