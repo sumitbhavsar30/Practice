96d1426d9fbb15c2042b3dcd5c38387bb49820c7
sumitbhavsar730 <sbhavsar730@gmail.com>
2021-03-29 22:59:43 +0530

    first project

---
+++ src/main/java/myproject1/JavaCoding.java
@@ -0,0 +1,12 @@
+package myproject1;
+
+import java.security.PublicKey;
+import java.util.*;
+
+public class JavaCoding {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My First Java Program");
+	}
+}
